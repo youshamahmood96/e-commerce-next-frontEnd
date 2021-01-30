b8dc325565e265bd2fba2f652c158119830c67db
youshamahmood <youshamahmood96@gmail.com>
2021-01-30 06:37:22 +0600

    minor changes

--- components/Footer.js
+++ components/Footer.js
@@ -1,17 +1,6 @@
 export default () => (
     <footer>
-        <div>© 2020 - Alex The Entreprenerd - Development Sponsored by 
-        <a 
-            target="_blank" 
-            rel="noopener noreferrer" 
-            href="https://magic.link/"
-        > Magic </a>
+        <div>© 2020 -Yousha Mahmood - Developed for an online challenge 
         </div>
-        <a href="https://www.youtube.com/watch?v=385cpCpGRC0&utm_source=site">
-            <h4>
-                Learn to code this on youtube <img className="img__yt" src="/NextJS.jpg" />
-            </h4>
-                
-        </a>
     </footer>
 )
--- utils/urls.js
+++ utils/urls.js
@@ -1,7 +1,6 @@
-export const API_URL =
-  process.env.NEXT_PUBLIC_API_URL || "http://localhost:1338"
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:1338"
 
-export const MAGIC_PUBLIC_KEY = process.env.NEXT_PUBLIC_MAGIC_PUBLIC_KEY || 'pk_test_42B2064C668798B5'
+export const MAGIC_PUBLIC_KEY = process.env.NEXT_PUBLIC_MAGIC_PUBLIC_KEY || 'pk_test_7837604C820F0E9C'
 
 export const STRIPE_PK = process.env.NEXT_PUBLIC_STRIPE_PK || 'pk_test_42B2064C668798B5'
 
